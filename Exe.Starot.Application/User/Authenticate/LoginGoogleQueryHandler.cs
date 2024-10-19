68de782dde910805ce3dacf9c96d35834b3510c7
kong18 <thanh2000cong20000@gmail.com>
2024-10-19 14:32:23 +0700

    push login

--- Exe.Starot.Application/User/Authenticate/LoginGoogleQueryHandler.cs
+++ Exe.Starot.Application/User/Authenticate/LoginGoogleQueryHandler.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using Exe.Starot.Application.Common.Interfaces;
 using Exe.Starot.Domain.Common.Exceptions;
+using Exe.Starot.Domain.Entities.Base;
 using Exe.Starot.Domain.Entities.Repositories;
+using Exe.Starot.Infrastructure.Repositories;
 using MediatR;
 using System.Linq;
 
@@ -10,11 +13,14 @@ namespace Exe.Starot.Application.User.Authenticate
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
-
-        public LoginGoogleQueryHandler(IUserRepository userRepository, IMapper mapper)
+        private readonly IJwtService _jwtService;
+        private readonly ICustomerRepository _customerRepository;
+        public LoginGoogleQueryHandler(IUserRepository userRepository, IMapper mapper, IJwtService jwtService, ICustomerRepository customerRepository)
         {
             _userRepository = userRepository;
             _mapper = mapper;
+            _jwtService = jwtService;
+            _customerRepository = customerRepository;
         }
 
         public async Task<UserLoginDTO> Handle(LoginGoogleQuery request, CancellationToken cancellationToken)
@@ -23,11 +29,41 @@ namespace Exe.Starot.Application.User.Authenticate
 
             if (user == null)
             {
-                throw new NotFoundException("User does not exist!");
+                user = new UserEntity
+                {
+                    Email = request.Email,
+                    FirstName = string.Empty,
+                    LastName = string.Empty,
+                    Role = "Customer", // Default role as Customer
+                    Balance = 0,
+                    Phone = string.Empty,
+                    DateOfBirth = null,
+                    Gender = string.Empty,
+                    Image = string.Empty
+                };
+                _userRepository.Add(user);
+
+                var customer = new CustomerEntity
+                {
+                    UserId = user.ID,
+                    Membership = 0 // Default membership level
+                };
+
+                _customerRepository.Add(customer);
+
+                // Save the changes
+                await _userRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
+
+               
             }
+            var accessToken = _jwtService.CreateToken(user.ID, user.Role, user.Email, user.LastName);
+            var refreshToken = _jwtService.GenerateRefreshToken();
+
+            await _userRepository.UpdateRefreshTokenAsync(user, refreshToken, DateTime.UtcNow.AddDays(30));
 
             var userLoginDto = _mapper.Map<UserLoginDTO>(user);
-
+            userLoginDto.Token = accessToken;
+            userLoginDto.RefreshToken = refreshToken;
             return userLoginDto;
         }
     }
